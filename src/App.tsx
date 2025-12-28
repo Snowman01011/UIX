7126d15f15b1550313ae855af07d38e4ad61aca0
Snowman01011 <snowygot36@gmail.com>
2025-12-28 16:32:08 +0545

    react Routerbugged #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router";
+import Navbar from './components/NavBar';
+import Home from './Pages/Home';
+import Courses from './Pages/Courses';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
 function App() {
   return (
     <>
-    <h2>Hello </h2>
+      <Navbar />
+
+       <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/Courses" element={<Courses />}/>
+          <Route path="/About" element={<About />}/>
+          <Route path="/Contact" element={<Contact />}/>
+        </Routes>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import './global.css';
+
+function NavBar(){
+    <>
+        <nav>
+            <Link 
+                to="/"
+                className=""
+            >
+            
+            </Link>
+        </nav>
+    </>
+}
+
+export default NavBar;
