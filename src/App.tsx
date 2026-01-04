c8f0009fec93712dbeca3198699e9ee059438891
snowmancodes <snowmancodes@gmail.com>
2026-01-05 01:20:15 +0545

    fixed react-router-dom problem during deployment #6

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 
-// component pages
+// pages
 import Home from './pages/Home'
 import Courses from './pages/Courses'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
-// component
+// components
 import Navbar from './components/NavBar'
 
-//  Css
+// CSS
 import './App.css'
 
+function App() {
+  return (
+    <div>
+      <Navbar />
 
-function App(){
-  return(
-    <>
-      <div 
-        className=''
-      >
-        <Navbar />
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Courses' element={<Courses />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Contact' element={<Contact />} />
-        </Routes>
-      </div>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 
-export default App;
+export default App
